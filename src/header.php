741cf9ca6727b4daaa0a10a1b5126e3c606ab756
rachciar <rach.ciarlante@gmail.com>
2023-09-06 12:03:30 -0400

    Header

--- src/header.php
+++ src/header.php
@@ -4,22 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/header.css">
     <script defer src="js/header.js"></script>
 </head>
 <body>
 
-<nav class="nav">
-         <input id="menu-toggle" type="checkbox" />
-            <label class="menu-button-container" for="menu-toggle">
+<nav>
+    <input id="menu-toggle" type="checkbox" />
+    <label class="menu-button-container" for="menu-toggle">
         <div class="menu-button"></div>
       </label>
+    <div class = 'menu'>
     <a href="#">Home</a>
     <a href="#">Services</a>
     <a href="#">About</a>
     <a href="#">Portfolio</a>
     <a href="#">Contact</a>
     <a href="#">Links</a>
+    </div>
+    <div class = 'social'>
+      <div class="tooltip">
+    <a href="https://www.linkedin.com/in/rachelciarlante/" target="_blank"><i class="fa fa-linkedin"></i></a>
+    <span class="tooltiptext">LinkedIn</span>
+    </div>
+    <div class="tooltip">
+    <a href="https://github.com/RachCiar" target="_blank"><i class="fa fa-github"></i></a>
+    <span class="tooltiptext">GitHub</span>
+    </div>
+    <div class="tooltip">
+    <a href="https://www.instagram.com/rae_ciarlante/" target="_blank"><i class="fa fa-instagram"></i></a>
+    <span class="tooltiptext">Instagram</span>
+    </div>
+    </div>
+
 </nav>
 
     
